36231b2f3cce2ad8276b8ae90a9e625fbbb6d30a
Yaacov Tarko <yaacov.tarko@gmail.com>
2017-03-07 16:10:26 -0800

    Added skeleton for password encryption

--- crypto.js
+++ crypto.js
@@ -7,18 +7,53 @@ var secrets = require('./secrets.json'); //this is the top secret file with all 
 
 var key = secrets.supersecretkey;
 
-encrypt_file(var inputfile, var outputfile)
-{
+var encrypt_file = function(inputfile, outputfile){
 	encryptor.encrypt_file(inputfile, outputfile, key, function(err){ 
 		//encryption complete
+		if(err){
+			console.log('encryption failed');
+		}
 	});
-}
+};
 
-decrypt_file(var inputfile, var outputfile)
-{
+var decrypt_file = function(inputfile, outputfile){
 	encryptor.decrypt_file(inputfile, outputfile, key, function(err){
 		//decryption complete
+		if(err){
+			console.log('encryption failed');
+		}
+	});
+};
+
+//ENCRYPT PASSWORDS
+var bcrypt = require('bcrypt');
+const saltRounds = 12; //make this bigger if you're paranoid
+
+var hash_password = function(plaintextpassword, vaultname){
+	bcrypt.hash(plaintextpassword, saltRounds, function(err, hash) {
+		if(err){
+			console.log('failed to hash password');
+			return false;
+		}
+		else{
+			//store hash in password database, associated with the vaultname field 
+			return true;
+		}
 	});
 }
 
-//ENCRYPT PASSWORDS (the hard part)
+var hash = ""; 
+var compare_password = function(plaintextpassword, vaultname){
+	var hash = ""; //this should be loaded from the database associated with the vaultname
+	bcrypt.compare(plaintextpassword, hash).then(function(result){
+		if(result){
+			//the passwords are the same 
+		}
+		else{
+			//the passwords are different
+		}
+	});
+}
+
+
+
