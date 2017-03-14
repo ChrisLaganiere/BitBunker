09c30a354e8063ddab651379b6a33b53cd7aeffa
chrislaganiere <chrislaganiere@gmail.com>
2017-03-13 20:30:03 -0700

    update app.js with routes

--- app.js
+++ app.js
@@ -22,31 +22,149 @@ app.use(bodyParser.json());
 // Return:
 //  {success: true} (opened vault for user session)
 /// {success: false, reason: "..."} (failed to open vault)
-app.post('/openvault', function(req, res) {
-	res.send(JSON.stringify({"success": true}));
-});
+var openvault = function(params, req, res) {
+	var vault = params['vault'];
+	var password = params['password'];
+	console.log("user tried to open", vault);
 
-app.post('/createvault', function(req, res) {
-	res.send(JSON.stringify({"success": true}));
-});
+	if (vault && password) {
+		// got vault and password
+		database.openvault(vault, password, function(success) {
+			if (success) {
+				res.json({"success": true});
+				// TO DO: chris
+			} else {
+				res.json({"success": false, "reason": "Database couldn't open vault"});
+				// TO DO: chris
+			}
+		});
+	} else {
+		// missing params
+		res.json({"success": false, "reason": "missing params..."});
+	}
+};
 
-app.post('/addfile', function(req, res) {
-	res.send(JSON.stringify({"success": true}));
-});
+// -> /createvault (POST)
+//   - PARAM: vault: string name of the vault to open
+//   - PARAM: password: string plaintext password
+// Return:
+//  {success: true} (opened vault for user session)
+/// {success: false, reason: "..."} (failed to open vault)
+var createvault = function(params, req, res) {
+	var vault = params['vault'];
+	var password = params['password'];
+	console.log("user tried to create", vault);
 
-app.post('/getfile', function(req, res) {
-	res.send(JSON.stringify({"success": true}));
-});
+	if (vault && password) {
+		// got vault and password
+		database.createvault(vault, password, function(success) {
+			if (success) {
+				res.json({"success": true});
+				// TO DO: chris
+			} else {
+				res.json({"success": false, "reason": "Database couldn't create vault"});
+				// TO DO: chris
+			}
+		});
+	} else {
+		// missing params
+		res.json({"success": false, "reason": "missing params..."});
+	}
+};
 
-app.post('/deletefile', function(req, res) {
-	res.send(JSON.stringify({"success": true}));
-});
+// -> /addfile (POST)
+//	 - PARAM: filename (name of the file you want to add)
+//	 - PARAM: filepath (path to the file)
+//   - PARAM: vault: string name of the vault to open
+// Return:
+//  {success: true} (if file is added)
+/// {success: false, reason: "..."} (failed to add file //adding file to vault that isn't open)
+var addfile = function(params, req, res) {
+	var vault = params['vault'];
+	var filename = params['filename'];
+	// var content = params['content'];
+	// var filepath = req.body['filepath'];
+	var filepath = 'ayyy.txt';
+	console.log("user tried to add", filename);
+
+	if (vault && filename && filepath) {
+		// got vault and password
+		database.addfile(vault, filename, filepath, function(success) {
+			if (success) {
+				res.json({"success": true});
+				// TO DO: chris
+			} else {
+				res.json({"success": false, "reason": "Database couldn't add file because file path is incorrect or vault isn't open"});
+				// TO DO: chris
+			}
+		});
+	} else {
+		// missing params
+		res.json({"success": false, "reason": "missing params..."});
+	}
+};
+
+// -> /getfile (POST)
+//	 - PARAM: filename (name of file you want to retreive)
+//   - PARAM: vault: string name of the vault to open
+// Return:
+//  {success: true, content: "...", filename: "..."} (if you retrieved the file)
+/// {success: false, reason: "..."} (if the file isn't in the vault)
+var getfile = function(params, req, res) {
+	var vault = params['vault'];
+	var filename = params['filename'];
+	console.log("user tried to get", filename);
+
+	if (vault && filename) {
+		// got vault and password
+		database.getfile(vault, filename, function(success) {
+			if (success) {
+				res.json({"success": true, "filename": filename " was successfully retrieved"});
+				// TO DO: chris
+			} else {
+				res.json({"success": false, "reason": "Database couldn't get file because file isn't in vault"});
+				// TO DO: chris
+			}
+		});
+	} else {
+		// missing params
+		res.json({"success": false, "reason": "missing params..."});
+	}
+};
+
+// -> /deletefile (POST)
+//	 - PARAM: filename (name of file you want to delete)
+//   - PARAM: vault: string name of the vault to open
+// Return:
+//  {success: true} (if file is deleted)
+/// {success: false, reason: "..."} (if file isn't in vault)
+var deletefile = function(params, req, res) {
+	var vault = params['vault'];
+	var filename = params['filename'];
+	console.log("user tried to delete", filename);
+
+	if (vault && filename) {
+		// got vault and password
+		database.getfile(vault, filename, function(success) {
+			if (success) {
+				res.json({"success": true});
+				// TO DO: chris
+			} else {
+				res.json({"success": false, "reason": "Database couldn't delete file because file isn't in vault"});
+				// TO DO: chris
+			}
+		});
+	} else {
+		// missing params
+		res.json({"success": false, "reason": "missing params..."});
+	}
 
 
+/*****/
+
+// actual route, with encrypted body payload
 app.post('/action', function(req, res) {
 	console.log(req.body);
-	// TODO: implement action
-	res.json({"success": true});
 
 	var action = "";
 	var params = {};
@@ -56,7 +174,6 @@ app.post('/action', function(req, res) {
 		var aeskey = "passwordpasswordpasswordpassword"
 		var iv = "drowssapdrowssap"
 		var decrypted = secure.decrypt(aeskey, iv, bunkerData);
-		console.log(decrypted);
 		if (decrypted) {
 			var pattern = /([^;=]*)=([^;=]*);/g;
 			var match = pattern.exec(decrypted);
@@ -77,6 +194,21 @@ app.post('/action', function(req, res) {
 	}
 
 	console.log('executing', action, params);
+	switch(action) {
+		case "openvault":
+			openvault(params, req, res);
+		case "createvault":
+			createvault(params, req, res);
+		case "addfile":
+			addfile(params, req, res);
+		case "getfile":
+			getfile(params, req, res);
+		case "deletefile":
+			deletefile(params, req, res);
+		default:
+			console.log("unknown action, ignoring");
+			res.json({"success": false, "reason": "unknown action: " + action});
+	}
 });
 
 /****/
